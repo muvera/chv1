be7d84109c0496a24c710de3a256cc36c46a16c8
vagrant <vagrant@precise64.(none)>
2014-11-04 19:12:46 +0000

    dbb

---
+++ app/views/pages/ediblesoft.blade.php
@@ -0,0 +1,69 @@
+<div itemscope itemtype="http://schema.org/WebApplication">
+
+<img itemprop="image" src="/images/ediblesoft.png" />
+<span itemprop="name">Edible Soft 1.0</span>
+<div itemprop="author" itemscope itemtype="http://schema.org/Organization">
+<a itemprop="url" href="http://cakehollywood.com"><span itemprop="name">CakeHollywood</span></a>
+</div>
+
+<span itemprop="description">Design edible cake images online from your camera photos. You can print, personalize a cake, cupcake, cookies and more.
+</span>
+
+CONTENT RATING: <span itemprop="contentRating">Creative</span>
+UPDATED: <time itemprop="datePublished" datetime="2014-09-24">Sep 24, 2014</time>
+<link itemprop="SoftwareApplicationCategory" href="http://schema.org/Application"/>
+CATEGORY: <span itemprop="SoftwareApplicationSubCategory">Brain & Puzzle</span>
+SIZE: 14M
+<meta itemprop="fileSize" content="14680064"/>
+INSTALLS: <meta itemprop="interactionCount" content=”UserDownloads:100000"/>100,000 - 500,000
+
+RATING:
+<div itemprop="aggregateRating" itemscope itemtype="http://schema.org/AggregateRating">
+ <span itemprop="ratingValue">4.6</span>(
+ <span itemprop="ratingCount">8864</span>)
+ <meta itemprop="reviewCount" content="3317" />
+</div>
+
+<div itemprop="offers" itemscope itemtype="http://schema.org/Offer">
+ Price: <span itemprop="price">$1.00</span>
+ <meta itemprop="priceCurrency" content="USD" />
+ <link itemprop="availability" href="http://schema.org/InStock" />INSTALL
+</div>
+
+
+Reviews:
+
+<div itemprop="reviews" itemscope itemtype="http://schema.org/Review">
+ <span itemprop="reviewRating">3</span> stars -
+ <b>"<span itemprop="name">A masterpiece</span>" </b>
+ by <span itemprop="author">Winston</span>,
+ Written on <time itemprop="publishDate" datetime="2011-07-13">July 13, 2011</time>
+ <span itemprop="reviewBody">Finally this amazing game has come to Android! 
+Outstanding game and very cheap for how awesome it is. </span>
+</div>
+
+<div itemprop="reviews" itemscope itemtype="http://schema.org/Review">
+ <span itemprop="reviewRating">5</span> stars -
+ <b>"<span itemprop="name">Love this game!</span>" </b>
+
+ by <span itemprop="author">Winston</span>,
+ Written on <time itemprop="publishDate" datetime="2011-08-22">August 22, 2011</time>
+ <span itemprop="reviewBody">I have three starred every box in this game cuz 
+I can't get enough of it. !</span></div>
+…
+
+
+THIS APPLICATION HAS ACCESS TO THE FOLLOWING:
+<span itemprop="permissions">YOUR LOCATION. COARSE (NETWORK-BASED) LOCATION
+Access coarse location sources such as the cellular network database to determine an approximate 
+device location, where available. Malicious applications can use this to determine approximately where you are.</span>
+<span itemprop="permissions">NETWORK COMMUNICATION
+FULL INTERNET ACCESS
+Allows an application to create network sockets.
+</span>
+<span itemprop="permissions">PHONE CALLS
+READ PHONE STATE AND IDENTITY
+Allows the application to access the phone features of the device. An application with this permission can determine the phone number 
+and serial number of this phone, whether a call is active, the number that call is connected to and the like.
+</span>
+</div>
